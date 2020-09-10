ec2a71b515c4b4d2f92a678da37f3a104c85d220
Carlos <carlosvaghetti01@gmail.com>
2020-09-10 18:54:19 -0300

    add JS part, convert values and show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 import "../stylesheets/application"
 
+require('./exchanges')
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
